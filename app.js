4e6e3dd8366a9f12b4b8db10d0f6570ee825705c
Pablo Leal <pabloleal7@hotmail.com>
2022-01-27 12:13:27 -0300

    yes

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const https = require("https");
+const request = require("request");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"))
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html")
+})
+
+app.post("/", function (req, res) {
+
+    const mailChimpApiKey = "dc7c155dd040950532a36170b7b4c351-us20"
+
+
+    const firstName = req.body.firstName
+    const lastName = req.body.lastName
+    const email = req.body.email
+
+    const data = {
+        members: [{
+
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+
+                FNAME: firstName,
+                LNAME: lastName,
+
+            }
+        }]
+    }
+
+    const jsonData = JSON.stringify(data)
+    const url = "https://us20.api.mailchimp.com/3.0/lists/fe4197a2d2"
+    // const audianceID = "fe4197a2d2"
+    const options = {
+        method: "POST",
+        auth: "pabloleal7:" + mailChimpApiKey,
+    }
+
+    const request = https.request(url, options, function (response) {
+
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/sucess.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html")
+        }
+
+        response.on("data", (data) => {
+                console.log(JSON.parse(data));
+            })
+    })
+
+    request.write(jsonData)
+    request.end()
+
+    // res.send(fistName + lastName + email)
+})
+
+app.post("/failure", function(req, res){
+    res.redirect("/")
+})
+
+
+
+const server = app.listen(process.env.port, function () {
+    console.log("Server On"+ server.address().port);
+});
